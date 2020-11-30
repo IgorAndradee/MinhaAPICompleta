88e412b4e682631fb50b9644c37c1ecdc3c35803
Igor Andrade Silva <igor.andrade1013@gmail.com>
2020-11-30 07:28:25 -0300

    Setup - Controllers e Startup

---
+++ MinhaAPICompleta/src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public abstract class MainController : ControllerBase
+    {
+
+    }
+}
